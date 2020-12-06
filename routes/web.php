31137d3680563116ff739ce3e2d9e3d4e9b27a3e
Biswajit Biswas <jfbiswajit@gmail.com>
2020-12-06 11:58:27 +0600

    Add: foo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/send_dat', function(HttpRequest $request) {
+  dd($request);
 });
